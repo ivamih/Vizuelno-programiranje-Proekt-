c9a6b010b4217f04c04a6cc83b323ff88b51a523
ivamih <ivamihajlovska@hotmail.com>
2017-05-25 02:26:59 +0200

    First commit
    Osnovni funkcionalnosti na igrata

---
+++ gameProject/Game_project/Assets/C# codes/Cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public float paddleSpeed = 1f;
+
+
+    private Vector3 playerPos = new Vector3(0, 1, 0);
+
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 9f), 1, 0f);
+        transform.position = playerPos;
+
+    }
+}
---
+++ gameProject/Game_project/Assets/C# codes/Death.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Death : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        GM.instance.LoseLife();
+    }
+}
