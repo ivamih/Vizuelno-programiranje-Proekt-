3142710f5223e48d4976616758c6455e7b474cdb
Jane Lameski <jane.lameski96@gmail.com>
2017-05-28 00:49:18 +0200

    Commit Posleden
    KrsteSKA

--- Game_project/Assets/C# codes/Open2.cs
+++ Game_project/Assets/C# codes/Open2.cs
@@ -6,7 +6,7 @@ public class Open2 : MonoBehaviour {
 
 	public void Open_2()
     {
-        Application.OpenURL("https://finkiukim-my.sharepoint.com/personal/jane_lameski_students_finki_ukim_mk/_layouts/15/guestaccess.aspx?docid=120a122aa34194b6199bd138639e0f95b&authkey=AfQpCSxcI3ChXx-J-Ctwylw");
+        Application.OpenURL("https://finkiukim-my.sharepoint.com/personal/jane_lameski_students_finki_ukim_mk/_layouts/15/guestaccess.aspx?docid=17d3df48f907c422a97824f6b789909f7&authkey=AYhRevBM7Qg-YsB2mfO6OeA");
 
     }
 }
